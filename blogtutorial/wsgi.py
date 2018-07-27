3e4488ec8202a6d06cf82ed1d4352953a1527b9b
Sunil Kumar <sunil.kumar@nescode.com>
2018-07-27 08:55:46 +0530

    [Update]Remaned project

--- blogtutorial/wsgi.py
+++ blogtutorial/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for komalblog project.
+WSGI config for blogtutorial project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "komalblog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogtutorial.settings")
 
 application = get_wsgi_application()
